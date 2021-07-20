180b098507edcc5c4a7c7243003598464e629f2c
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-07-20 15:48:17 -0400

    Perform first test

--- QuantumAIA/QuantumAIA.py
+++ QuantumAIA/QuantumAIA.py
@@ -1,6 +1,22 @@
 import qsharp
-qsharp.reload()
-print(qsharp.get_available_operations())
+from collections import Counter
+from NEQR import EncodeNEQRResult
 
-from NEQR import TestRandom
-print(TestRandom.simulate())
+image = [
+    [0, 16, 32, 48],
+    [64, 80, 96, 112],
+    [128, 144, 160, 176],
+    [192, 208, 224, 255],
+]
+
+results = Counter()
+for i in range(1024):
+    result = EncodeNEQRResult.simulate(image=image)
+    color = int(''.join([str(l) for l in result[7::-1]]), 2)
+    y = int(''.join([str(l) for l in result[10:7:-1]]), 2)
+    x = int(''.join([str(l) for l in result[13:10:-1]]), 2)
+
+    if y < 4 and x < 4:
+        results[(color, y, x)] += 1
+
+print(results)
